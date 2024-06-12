083a6dfdbff10a0c9451243725f6d0cf5a4d5ef8
gowthamy <gowthamy423@gmail.com>
2024-06-12 14:03:30 +0530

    solution path issue fix

--- obj/Debug/net8.0/ShortestPathConsole.AssemblyInfo.cs
+++ obj/Debug/net8.0/ShortestPathConsole.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShortestPathConsole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28e2a8aa424116320828c011464bbb1a1c610f5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShortestPathConsole")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShortestPathConsole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
